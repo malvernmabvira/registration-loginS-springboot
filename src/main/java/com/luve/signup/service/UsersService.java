8016701318968246edd5d9b30d62bd8d22988f60
malvernmabvira <malvernmabvira@gmail.com>
2022-04-28 03:46:33 +0300

    Configured spring security feature in SecurityConfig class

--- src/main/java/com/luve/signup/service/UsersService.java
+++ src/main/java/com/luve/signup/service/UsersService.java
@@ -1,9 +1,11 @@
 package com.luve.signup.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.luve.signup.model.Users;
 import com.luve.signup.web.dto.UsersRegistrationDto;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
 	
 	Users save(UsersRegistrationDto regDto);
 
--- src/main/java/com/luve/signup/service/UsersServiceImp.java
+++ src/main/java/com/luve/signup/service/UsersServiceImp.java
@@ -2,6 +2,8 @@ package com.luve.signup.service;
 
 import java.util.Arrays;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.luve.signup.model.Roles;
@@ -24,6 +26,12 @@ public class UsersServiceImp implements UsersService {
 		Users user = new Users(regDto.getFirstName(), regDto.getLastName(), regDto.getEmail(), 
 				regDto.getPassword(), Arrays.asList(new Roles("ROLE_USER")));
 		return repo.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
